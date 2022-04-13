7a2fc42c22d627f749ffbb2433e36f286b16b9f5
MrFandore <99207909+MrFandore@users.noreply.github.com>
2022-04-13 13:56:38 +0300

    Add files via upload

---
+++ Includes/Globalvar.h
@@ -0,0 +1,38 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1649227629_2_
+#define _BUR_1649227629_2_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_GLOBAL plcbit RUN;
+_GLOBAL signed short OTR;
+_GLOBAL unsigned short LCR;
+_GLOBAL signed short Speed;
+_GLOBAL unsigned short CMDD;
+_GLOBAL unsigned short ETAD;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1649227629_2_ */
+
---
+++ Includes/Program/Variablesvar.h
@@ -0,0 +1,37 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1649233902_1_
+#define _BUR_1649233902_1_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_BUR_LOCAL unsigned char Onlinerun;
+_BUR_LOCAL plcbit Reset;
+_BUR_LOCAL plcbit speedUp;
+_BUR_LOCAL plcbit speedDown;
+_BUR_LOCAL unsigned short STATE;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Program/Variables.var\\\" scope \\\"local\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1649233902_1_ */
+
